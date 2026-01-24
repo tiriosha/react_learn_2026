bfe9b661434dc796692c4cda1fbd1067e15cfce2
tiriosha <agruman@mail.ru>
2026-01-24 14:48:56 +0500

    Players name and symbol added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 function App() {
-  return <p>commingsoon</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="players-name">Player 1</span>
+              <span className="players-symbol"> X</span>
+            </span>
+            <button>EDIT</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="players-name">Player 2</span>
+              <span className="players-symbol"> O</span>
+            </span>
+            <button>EDIT</button>
+          </li>
+        </ol>
+        Game Board
+      </div>
+    </main>
+  );
 }
 
 export default App;
