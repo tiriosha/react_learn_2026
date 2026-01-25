969b1b8b0d4f0c3411fbf2c6193b3e50e786f757
tiriosha <agruman@mail.ru>
2026-01-25 14:09:27 +0500

    In players component made some interactivity with useState and hadleclick function

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="players-name">{name}</span>
+        {!isEditing ? (
+          <span className="players-name">{name}</span>
+        ) : (
+          <input type="text" defaultValue={name} required />
+        )}
         <span className="players-symbol"> {symbol}</span>
       </span>
-      <button>EDIT</button>
+      <button onClick={handleEditClick}>EDIT</button>
     </li>
   );
 }
